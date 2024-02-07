198ef52ade8bb3879793a820d0758ae6441af8d4
brendahenriquez <bhenriqu@masonlive.gmu.edu>
2024-02-07 02:37:42 -0500

    configuration to build

--- next.config.js
+++ next.config.js
@@ -65,6 +65,7 @@ module.exports = () => {
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     eslint: {
+      output: 'export',
       dirs: ['app', 'components', 'layouts', 'scripts'],
     },
     images: {
