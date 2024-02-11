0d41f6fc31b8d3e5b86160e9e8def060cbb420ba
brendahenriquez <bhenriqu@masonlive.gmu.edu>
2024-02-10 23:51:29 -0500

    update next.config.js and  yarn build-and-export

--- next.config.js
+++ next.config.js
@@ -65,7 +65,6 @@ module.exports = () => {
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     eslint: {
-      output: 'export',
       dirs: ['app', 'components', 'layouts', 'scripts'],
     },
     images: {
