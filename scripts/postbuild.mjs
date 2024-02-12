0eca8360609a41267823305d9ec72ce12b70bd33
brendahenriquez <bhenriqu@masonlive.gmu.edu>
2024-02-11 21:23:06 -0500

    remove out from gitignore & yarn build-and-export

--- scripts/postbuild.mjs
+++ scripts/postbuild.mjs
@@ -1,7 +1,21 @@
-import rss from './rss.mjs'
+// Import the entire fs-extra package as a CommonJS module
+import pkg from 'fs-extra';
+const { copySync } = pkg;
 
 async function postbuild() {
-  await rss()
+  // Your existing postbuild logic
+  // ...
+
+  // Copy the static files from the .next output directory to the out directory
+  try {
+    copySync('./.next/server/pages', './out', {
+      overwrite: true,
+      recursive: true,
+    });
+    console.log('Static files have been copied to the out directory.');
+  } catch (err) {
+    console.error('Error copying static files:', err);
+  }
 }
 
-postbuild()
+postbuild();
